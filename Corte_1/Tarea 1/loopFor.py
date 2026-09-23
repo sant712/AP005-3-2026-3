7abb18e4de16c5a40e4cbab72d9c2464c8cf4081
sant712 <santpartorres712@gmail.com>
2026-09-23 12:55:19 -0500

    Enhance loopFor.py with comments for clarity
    Added comments to clarify the purpose of each code section.

--- Corte_1/Tarea 1/loopFor.py
+++ Corte_1/Tarea 1/loopFor.py
@@ -1,9 +1,9 @@
-import time
+import time                     # Importa la librería para manejar tiempos y pausas
 
-cadena = 'Python'
+cadena = 'Python'               # Define la cadena de texto a recorrer
 
-for letra in cadena:
-	if letra == 't':
-		continue
-	print(letra)
-	time.sleep(1)
+for letra in cadena:            # Itera letra por letra en la palabra
+	if letra == 't':            # Si la letra actual es 't'...
+		continue                # Omite esta iteración y pasa a la siguiente letra
+	print(letra)                # Imprime la letra actual
+	time.sleep(1)               # Detiene la ejecución del programa durante 1 segundo
