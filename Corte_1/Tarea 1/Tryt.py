05ad957f151e99cc8f15a204f298db81375d9838
sant712 <santpartorres712@gmail.com>
2026-09-23 12:53:39 -0500

    Update Tryt.py

--- Corte_1/Tarea 1/Tryt.py
+++ Corte_1/Tarea 1/Tryt.py
@@ -1,2 +1,2 @@
-my_cool_variable = 7 + 8 == 11
-print(my_cool_variable)
+my_cool_variable = 7 + 8 == 11  # Realiza la suma (15) y evalúa si es igual a 11, guardando False
+print(my_cool_variable)         # Muestra el valor de la variable (en este caso, False)
