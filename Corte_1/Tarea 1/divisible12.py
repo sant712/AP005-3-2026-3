b1d6b51df9337e87de189563a462b5435061d77e
sant712 <santpartorres712@gmail.com>
2026-09-23 12:53:52 -0500

    Add comments to divisible12.py for clarity
    Added comments to explain the logic of the code.

--- Corte_1/Tarea 1/divisible12.py
+++ Corte_1/Tarea 1/divisible12.py
@@ -1,4 +1,4 @@
-for i in range(100, 301):
-	if (i % 12) != 0:
-		continue
-	print(i)
+for i in range(100, 301):       # Itera desde el número 100 hasta el 300
+	if (i % 12) != 0:           # Si el residuo de dividir entre 12 es diferente de 0...
+		continue                # Salta a la siguiente iteración, ignorando el resto del bucle
+	print(i)                    # Imprime el número solo si es múltiplo de 12
