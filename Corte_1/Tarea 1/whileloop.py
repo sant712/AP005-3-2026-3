86f1c9c81379e4766738d9d0ebec7ab5a43e2548
sant712 <santpartorres712@gmail.com>
2026-09-23 12:57:26 -0500

    Refactor while loop with comments
    Updated while loop logic with comments for clarity.

--- Corte_1/Tarea 1/whileloop.py
+++ Corte_1/Tarea 1/whileloop.py
@@ -1,6 +1,5 @@
-for i in range(1,6):
-    while i <= 4:
-        i += 1
-        print(i)
-    break    
-    
+for i in range(1,6):            # Bucle externo que toma valores del 1 al 5
+    while i <= 4:               # Bucle interno que opera mientras 'i' sea menor o igual a 4
+        i += 1                  # Incrementa el valor de 'i' en 1
+        print(i)                # Imprime el valor actualizado de 'i'
+    break                       # Rompe y finaliza el bucle externo inmediatamente en la primera vuelta completa
