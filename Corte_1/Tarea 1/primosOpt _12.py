32987b8b50b4ff125bd39f6733a1839de0c9bd60
sant712 <santpartorres712@gmail.com>
2026-09-23 12:58:17 -0500

    Optimize prime checking with increased range and break
    Added comparison of optimization by increasing range to 100 and measuring cycles with and without break.

--- Corte_1/Tarea 1/primosOpt _12.py
+++ Corte_1/Tarea 1/primosOpt _12.py
@@ -1,1 +1,33 @@
+# 12) Comprobación de escala de la optimización aumentando el rango a 100
+tope_rango=100                  # Se incrementa el límite a evaluar
+ciclos_sin_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_sin_break += 1
+        if (n % div == 0):
+            primo = False
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_sin_break))
 
+ciclos_con_break = 0
+n = 0
+primo = True
+while (n < tope_rango):
+    for div in range(2, n):
+        ciclos_con_break += 1
+        if (n % div == 0):
+            primo = False
+            break               # Detiene el bucle antes de tiempo ahorrando iteraciones
+    if (primo):
+        print(n)
+    else:
+        primo = True
+    n += 1
+print('Cantidad de ciclos: ' + str(ciclos_con_break))
+print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
